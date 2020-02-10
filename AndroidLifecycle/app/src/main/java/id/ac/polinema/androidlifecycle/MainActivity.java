e9c53932a36188c9c52f525365e4475e31a44164
aruwansha <ilhamfebrianarwansyah@gmail.com>
2020-02-10 16:35:53 +0700

    Menyelesaikan TODO

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -14,39 +14,39 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		Toast.makeText(this, "Application On Start", Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Application On Start", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		Toast.makeText(this, "Application On Stop", Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Application On Stop", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-		Toast.makeText(this, "Application On Restart", Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Application On Restart", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		Toast.makeText(this, "Application On Resume", Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Application On Resume", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		Toast.makeText(this, "Application On Pause", Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Application On Pause", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		Toast.makeText(this, "Application On Destroy", Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Application On Destroy", Toast.LENGTH_SHORT).show();
+    }
 }
